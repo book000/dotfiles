dff3d649cb0e0c7e7619162f2516defe9c8d1594
Tomachi[Nuts] <tomachi@tomacheese.com>
2026-02-28 00:06:26 +0900

    feat: npm グローバルパッケージのパスを追加

--- home/dot_bashrc.d/executable_00-path.sh
+++ home/dot_bashrc.d/executable_00-path.sh
@@ -1,3 +1,6 @@
 # PATHの設定
 # $HOME/.local/bin をPATHの先頭に追加し、ユーザーごとのコマンドを優先的に実行できるようにします。
 export PATH="$HOME/.local/bin:$PATH"
+
+# npm グローバルパッケージのパス
+export PATH="$HOME/.npm-global/bin:$PATH"
---
+++ home/dot_zshrc.d/executable_00-path.zsh
@@ -0,0 +1,6 @@
+# PATH の設定
+# $HOME/.local/bin をPATHの先頭に追加し、ユーザーごとのコマンドを優先的に実行できるようにする
+export PATH="$HOME/.local/bin:$PATH"
+
+# npm グローバルパッケージのパス
+export PATH="$HOME/.npm-global/bin:$PATH"
