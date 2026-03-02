558378935754fb8ee0d8b630566ab58e2a960169
akubiusa [CinnamonUbuntuCC] <akubiusa@tomacheese.com>
2026-03-02 23:48:42 +0900

    fix: chezmoi コンフリクト時に自動で skip を選択

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd "$HOME" || exit
-sh -c "$(curl -fsSL get.chezmoi.io)" -- update
+# コンフリクト発生時に対話プロンプトが出た場合、自動で "s"（skip）を送信する
+yes "s" | sh -c "$(curl -fsSL get.chezmoi.io)" -- update
